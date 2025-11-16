48d579159e9f634cd00b64cde1368dfe99e89345
Aziz Rozmetov <argringo97@gmail.com>
2025-11-16 14:27:37 +0500

    saidbar is added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import { ref, provide } from 'vue'
+import { darkTheme, NConfigProvider, NSwitch } from 'naive-ui'
+
+// Global dark/light toggle holati
+const isDark = ref(false)
+
+// Boshqa componentlar (masalan, LeftSidebar) foydalanishi uchun
+provide('isDark', isDark)
 </script>
 
 <template>
-  <RouterView />
-</template>
+  <n-config-provider :theme="isDark ? darkTheme : null">
+    <!-- Dark/Light switch global joyda -->
+    <div style="padding: 10px">
+      <n-switch v-model:value="isDark">
+        {{ isDark ? 'Dark Mode' : 'Light Mode' }}
+      </n-switch>
+    </div>
 
-<style></style>
+    <!-- Sidebar RouterView -->
+    <RouterView name="LeftSidebar" />
+  </n-config-provider>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,38 +2,40 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AboutView from '@/pages/AboutView.vue'
 import HomeView from '@/pages/HomeView.vue'
 
-
 import NotFoundView from '@/pages/NotFoundView.vue'
 import LoginView from '@/pages/LoginView.vue'
 
 const routes = [
-    {
+  {
     path: '/home',
     name: 'home',
-    component: HomeView
+    components: {
+      LeftSidebar: () => import('@/components/SidebarMenuComponent.vue'),
+      default: () => import('@/pages/HomeView.vue'),
+    },
   },
 
   {
     path: '/about',
     name: 'about',
-    component: AboutView
+    component: AboutView,
   },
-    {
+  {
     path: '/login',
     name: 'login',
-    component: LoginView
+    component: LoginView,
   },
   // 404 sahifa uchun fallback
   {
     path: '/:pathMatch(.*)*',
     name: 'not-found',
-    component: NotFoundView
-  }
+    component: NotFoundView,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
